48ffa77df1906fc6e1e980879dcb2f6d4baf97a2
Dan Stevens <stevens@earthit.com>
2014-04-18 12:41:16 -0500

    Implement in: field matcher.

---
+++ lib/EarthIT/CMIPREST/FieldMatcher/In.php
@@ -0,0 +1,20 @@
+<?php
+
+class EarthIT_CMIPREST_FieldMatcher_In implements EarthIT_CMIPREST_FieldMatcher
+{
+	protected $values;
+	
+	public function __construct( array $values ) {
+		$this->values = $values;
+	}
+	
+	public function toSql( $fieldValueSql, $fieldType, &$params ) {
+		$values = array();
+		foreach( $this->values as $v ) {
+			$values[] = EarthIT_CMIPREST_Util::cast($v, $fieldType);
+		}
+		$paramName = EarthIT_DBC_ParameterUtil::newParamName('matchValues');
+		$params[$paramName] = $values;
+		return "{$fieldValueSql} IN {{$paramName}}";
+	}
+}
