6a69f88c591470c6fc4c8f14e0f435c90224a1f1
Dan Stevens <stevens@earthit.com>
2014-05-26 18:55:23 -0500

    Separate instance variable for storage in RESTerTest.

--- test/EarthIT/CMIPREST/RESTerTest.php
+++ test/EarthIT/CMIPREST/RESTerTest.php
@@ -3,6 +3,7 @@
 class EarthIT_CMIPREST_RESTerTest extends PHPUnit_Framework_TestCase
 {
 	protected $dbAdapter;
+	protected $storage;
 	protected $rester;
 	
 	public function setUp() {
@@ -14,9 +15,9 @@ class EarthIT_CMIPREST_RESTerTest extends PHPUnit_Framework_TestCase
 		}
 		$dbConfig = EarthIT_JSON::decode($dbConfigJson);
 		$this->dbAdapter = Doctrine\DBAL\DriverManager::getConnection($dbConfig);
+		$this->storage = new EarthIT_CMIPREST_PostgresStorage($this->dbAdapter, new EarthIT_DBC_PostgresNamer());
 		$this->rester = new EarthIT_CMIPREST_RESTer(array(
-			'dbAdapter' => $this->dbAdapter,
-			'dbNamer' => new EarthIT_DBC_PostgresNamer(),
+			'storage' => $this->storage,
 			'schema' => (require 'test-schema.php')
 		));
 	}
