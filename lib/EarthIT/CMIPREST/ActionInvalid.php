e8f3a1230f995476430323ee8359d93b153cf81a
Dan Stevens <stevens@earthit.com>
2014-11-06 17:25:36 -0600

    Add comment about ActionInvalid constructor parameters.

--- lib/EarthIT/CMIPREST/ActionInvalid.php
+++ lib/EarthIT/CMIPREST/ActionInvalid.php
@@ -5,6 +5,17 @@ class EarthIT_CMIPREST_ActionInvalid extends Exception
 	protected $action;
 	protected $errorDetails;
 	
+	/**
+	 * @param $action The action that was invalid.
+	 * @param $errorDetails a list of error structures like
+	 *   {
+	 *     "class": "ClientError/ActionInvalid",
+	 *     "message": "You did it wrong",
+	 *     "resourceClassName": "rinsed thingamabob",
+	 *     "resourceInstanceId": 37,
+	 *     "notes": [ "Your thingamabob is in an invalid state and you should probably reset your database" ]
+	 *   }
+	 */
 	public function __construct( EarthIT_CMIPREST_UserAction $action, array $errorDetails=array() ) {
 		$this->action = $action;
 		$this->errorDetails = $errorDetails;
