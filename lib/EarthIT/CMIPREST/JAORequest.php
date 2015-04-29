c8e040f19b59b1f156787c015472abc2c1e7c315
Dan Stevens <stevens@earthit.com>
2015-04-28 23:46:25 -0500

    Better support JAO POST/PUT/PATCH requests.

--- lib/EarthIT/CMIPREST/JAORequest.php
+++ lib/EarthIT/CMIPREST/JAORequest.php
@@ -50,30 +50,82 @@ class EarthIT_CMIPREST_JAORequest
 			$fieldsByJsonApiName[call_user_func($nameFormatter, $f->getName())] = $f;
 		}
 		
-		$data = array();
-		foreach( $bod as $k=>$v ) {
-			if( isset($fieldsByJsonApiName[$k]) ) {
-				$f = $fieldsByJsonApiName[$k];
-				$data[$f->getName()] = self::parseValue($v, $f->getType());
-			} else switch($k) {
-			case 'type':
-				$expectedTypeName = self::jaoTypeName($rc, $nameFormatter);
-				if( $expectedTypeName != $v ) {
-					throw new Exception("Type specified in data ('$v') does not match that expected for this URL ('$expectedTypeName')");
-				}
-				break;
-			case 'links':
-				//foreach( $v as $thing=>$stuff ) {
-				//}
-				// Ignore for now
-				// TODO: Don't ignore.
-			default:
-				$rcName = $rc->getName();
-				throw new Exception("Unrecognized $rcName attribute: '$k'");
+		$refInfoByJsonApiName = array();
+		foreach( $rc->getReferences() as $ref ) {
+			$jaoName = call_user_func($nameFormatter, $ref->getName());
+			$tfn = $ref->getTargetFieldNames();
+			$ofn = $ref->getOriginFieldNames();
+			/** field name to be found in 'linkages' => our (schema-form) field name */
+			$fmap = array();
+			for( $i=0; $i<count($tfn); ++$i ) {
+				$fmap[call_user_func($nameFormatter, $tfn)] = $ofn[$i];
 			}
+			$refInfoByJsonApiName[$jaoName] = array(
+				'targetJsonApiTypeName' => self::jaoTypeName($schema->getResourceClass($ref->getTargetClassName()), $nameFormatter),
+				'33fmap' => $fmap,
+			);
+		
 		}
 		
-		return $data;
+		// ^ It might be nice to cache all that stuff somewhere in case
+		// we're doing a lot of these.
+		
+		$isList = true;
+		for( $i=0; $i<count($bod); ++$i ) {
+			if( !array_key_exists($i, $bod) ) $isList = false;
+		}
+		
+		$bods = $isList ? $bod : array($bod);
+		
+		// So, uhm, this provides no way to update 1-many (where the
+		// object in question is the one) relationships.
+		// The semantics on what those would mean are, I suppose, documented
+		// at http://jsonapi.org/format/#crud-updating-to-many-relationships
+		
+		$items = array();
+		foreach( $bods as $bod ) {
+			$item = array();
+			foreach( $bod as $k=>$v ) {
+				if( isset($fieldsByJsonApiName[$k]) ) {
+					$f = $fieldsByJsonApiName[$k];
+					$item[$f->getName()] = self::parseValue($v, $f->getType());
+				} else switch($k) {
+				case 'type':
+					$expectedTypeName = self::jaoTypeName($rc, $nameFormatter);
+					if( $expectedTypeName != $v ) {
+						throw new Exception("Type specified in data ('$v') does not match that expected for this URL ('$expectedTypeName')");
+					}
+					break;
+				case 'links':
+					foreach( $v as $thing=>$stuff ) {
+						// TODO: Throw a 403? http://jsonapi.org/format/#crud-updating-resource-relationships
+						if( !isset($refInfoByJsonApiName[$thing]) ) continue; // Ignore the unrecognized links
+						$refInfo = $refInfoByJsonApiName[$thing];								
+						foreach( $stuff['linkage'] as $fmapKey => $v ) {
+							if( $fmapKey == 'type' ) {
+								if( $v != $refInfo['targetJsonApiTypeName'] ) {
+									throw new Exception("Type specified in linkage ('$v') does not match that expected for '$thing' ('{$refInfo['targetJsonApiTypeName']}')");
+								}
+							} else {
+								$item[$refInfo['33fmap'][$fmapKey]] = $v;
+							}
+						}
+					}
+					break;
+				default:
+					$rcName = $rc->getName();
+					throw new Exception("Unrecognized $rcName attribute: '$k'");
+				}
+			}
+			$items[] = $item;
+		}
+		
+		return $items;
+	}
+	
+	protected static function firstAndOnly( array $stuff ) {
+		if( count($stuff) != 1 ) throw new Exception("Not exactly one thing in the stuff, here: ".var_export($stuff,true));
+		foreach( $stuff as $thing ) return $thing;
 	}
 	
 	public static function jaoRequestToUserAction( EarthIT_CMIPREST_JAORequest $req, EarthIT_Schema $schema, callable $nameFormatter ) {
@@ -93,14 +145,18 @@ class EarthIT_CMIPREST_JAORequest
 				return new EarthIT_CMIPREST_UserAction_GetItemAction($req->userId, $rc, $req->instanceId, array(), $opts);
 			}
 		case 'POST':
-			return new EarthIT_CMIPREST_UserAction_PostItemAction($req->userId, $rc,
-				self::parseContentData($req->content['data'], $rc, $schema, $nameFormatter), $opts);
+			$items = self::parseContentData($req->content['data'], $rc, $schema, $nameFormatter);
+			$item = self::firstAndOnly($items);
+			// TODO: Allow multi-posts
+			return new EarthIT_CMIPREST_UserAction_PostItemAction($req->userId, $rc, $item, $opts);
 		case 'PUT':
-			return new EarthIT_CMIPREST_UserAction_PostItemAction($req->userId, $rc, $req->instanceId,
-				self::parseContentData($req->content['data'], $rc, $schema, $nameFormatter), $opts);
+			$items = self::parseContentData($req->content['data'], $rc, $schema, $nameFormatter);
+			$item = self::firstAndOnly($items);
+			return new EarthIT_CMIPREST_UserAction_PutItemAction($req->userId, $rc, $req->instanceId, $item, $opts);
 		case 'PATCH':
-			return new EarthIT_CMIPREST_UserAction_PostItemAction($req->userId, $rc, $req->instanceId,
-				self::parseContentData($req->content['data'], $rc, $schema, $nameFormatter), $opts);
+			$items = self::parseContentData($req->content['data'], $rc, $schema, $nameFormatter);
+			$item = self::firstAndOnly($items);
+			return new EarthIT_CMIPREST_UserAction_PatchItemAction($req->userId, $rc, $req->instanceId, $item, $opts);
 		}
 		
 		throw new Exception( "Unsupported JAORequest: ".print_r($req,true));
